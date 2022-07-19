dc2e6bbe426fbec81d8790257bdb1aa3e06ae339
sandeeprewa <91anu.sandeep@gmail.com>
2022-07-19 08:06:53 +0530

    working for single client : Working code

--- src/main/java/com/keycloak/client/config/KeycloakConfig.java
+++ src/main/java/com/keycloak/client/config/KeycloakConfig.java
@@ -1,5 +1,16 @@
 package com.keycloak.client.config;
 
-public class KeycloakConfig {
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.springframework.context.annotation.Bean;
 
+@KeycloakConfiguration
+public class KeycloakConfig {
+	
+	@Bean
+	public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+	  return new KeycloakSpringBootConfigResolver();
+			  
+    }
+	
 }
--- src/main/java/com/keycloak/client/entrypoint/TenantSsoLoginEntrypoint.java
+++ src/main/java/com/keycloak/client/entrypoint/TenantSsoLoginEntrypoint.java
@@ -4,33 +4,78 @@ import java.security.Principal;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.keycloak.AuthorizationContext;
+//import org.keycloak.AuthorizationContext;
+import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.keycloak.adapters.OAuthRequestAuthenticator;
+//import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+//import org.keycloak.representations.AccessToken;
+//import org.keycloak.representations.AccessToken.Access;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TenantSsoLoginEntrypoint {
 	
+	OAuthRequestAuthenticator s;
 	
 	@GetMapping("/public/test")
 	public String publicTest(HttpServletRequest request) {
 		
-    final Principal principal = (Principal) request.getUserPrincipal();
+//    final Principal principal = (Principal) request.getUserPrincipal();
 
+    KeycloakPrincipal<KeycloakSecurityContext> principal = (KeycloakPrincipal)request.getUserPrincipal();
+    
+    System.out.println("principal.getName();"+ principal.getName());
+    
+    KeycloakSecurityContext kss=   principal.getKeycloakSecurityContext();
+
+  
     System.out.println("******** "+ principal);
+    System.out.println("******** "+ kss);
 	
 		return "public Resource";
 	}
 	
 	@GetMapping("/private/test")
 	public String privateTest(HttpServletRequest request) {
+		
 	    final Principal principal = (Principal) request.getUserPrincipal();
 
 	    System.out.println("******** "+ principal);
+	    System.out.println("principal.getName();"+ principal.getName());
+	    
+	    KeycloakPrincipal<KeycloakSecurityContext> principal1 = (KeycloakPrincipal)request.getUserPrincipal();
+	    
+	    System.out.println("principal.getName();"+ principal1.getName());
+	    
+	    KeycloakSecurityContext kss=   principal1.getKeycloakSecurityContext();
 
+	  
+	    System.out.println("******** "+ principal1);
+	    System.out.println("******** "+ kss);
 		
+/**	    
+	    KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) request.getUserPrincipal();        
+        KeycloakPrincipal principal11=(KeycloakPrincipal)token.getPrincipal();
+        KeycloakSecurityContext session = principal11.getKeycloakSecurityContext();
+        AccessToken accessToken = session.getToken();
+        String username = accessToken.getPreferredUsername();
+        String emailID = accessToken.getEmail();
+        String lastname = accessToken.getFamilyName();
+        String firstname = accessToken.getGivenName();
+        String realmName = accessToken.getIssuer();            
+        Access realmAccess = accessToken.getRealmAccess();
+        java.util.Set<String> roles = realmAccess.getRoles();
+        
+        System.out.println(username);
+        System.out.println(emailID);
+        System.out.println(lastname);
+        System.out.println(firstname);
+        System.out.println(realmName);
+        System.out.println(roles);
+**/
 		return "private Resource";
 	}
 
